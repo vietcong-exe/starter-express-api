8fbec39827b20f4f02f8cc1e97a65a069b5d59cb
vietcong-exe <156387611+vietcong-exe@users.noreply.github.com>
2024-02-14 05:51:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.post('/', (req, res) => {
+    const { username } = req.body
+    res.send(`Yo! ${username}`)
 })
 app.listen(process.env.PORT || 3000)
